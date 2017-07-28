bdecdb95d40b57a1fd999c3a3cacba0f4d8ac6f3
Chris Pan <yf.pan.chris@gmail.com>
2017-07-28 16:57:38 +1000

    v1.11

--- oderSys/ConversationHelper.cs
+++ oderSys/ConversationHelper.cs
@@ -11,34 +11,42 @@ namespace ConversationAPI
 {   
     public class ConversationHelper
     {
+        private readonly string _Server; //protect the url address
+        private readonly NetworkCredential _NetCredential; //protect the credential information
 
-        
-        private readonly string _Server;
-        private readonly NetworkCredential _NetCredential;
+        /// <summary>
+        /// constructor pass value of workSpaceID, userID, password
+        /// </summary>
+        /// <param name="workSpaceId">watson api workspace id</param>
+        /// <param name="userId">watson api user id</param>
+        /// <param name="password">watson api password id</param>
         public ConversationHelper(string workSpaceId, string userId, string password)
         {
             _Server = string.Format("https://gateway.watsonplatform.net/conversation/api/v1/workspaces/{0}/message?version={1}", workSpaceId, DateTime.Today.ToString("yyyy-MM-dd"));
             _NetCredential = new NetworkCredential(userId, password);
         }
+
+        /// <summary>
+        /// async method GetResponse post the value to the web and get return value
+        /// </summary>
+        /// <param name="input">what user input in the txtbox </param>
+        /// <param name="context"></param>
+        /// <returns></returns>
         public async  Task<string> GetResponse(string input, string context = null)
         {
             string req = null;
             if (string.IsNullOrEmpty(context)) req = "{\"input\": {\"text\": \"" + input + "\"}, \"alternate_intents\": true}";
             else req = "{\"input\": {\"text\": \"" + input + "\"}, \"alternate_intents\": true}, \"context\": \"" + context + "\"";
             var handler = new HttpClientHandler();
-
             handler.Credentials = _NetCredential;
-             
+            //post and when finished release the resource
             using (var client = new HttpClient(handler))
             {
                 var cont = new HttpRequestMessage();
-              
                 cont.Content = new StringContent(req.ToString(), Encoding.UTF8, "application/json");
-                var result = await client.PostAsync(_Server, cont.Content);
+                var result = await client.PostAsync(_Server, cont.Content);//post to the web
                 return await result.Content.ReadAsStringAsync();
             }
         }
-
-       
     }
 }
--- oderSys/OrderPanel.cs
+++ oderSys/OrderPanel.cs
@@ -14,11 +14,18 @@ namespace  ConversationAPI
 {
 
     public partial class OrderPanel : Form
-    {
+    {   
+        //new a COnversationHelper class to post value to the Watson and get the return value,
+        //pass on the workID, userID, password
         ConversationHelper helper = new ConversationHelper("a2bba1ef-9fa5-4002-a0c6-ffba34fd4983", "2c17fe46-d34b-4608-9f9b-03108554a9e8", "2W00upyXOUBb");
-        System.Timers.Timer t = new System.Timers.Timer(2000);
         string input = "";
 
+        /// <summary>
+        /// OrderPanel class constructor
+        /// initialize the components on the form
+        /// initialize the text of the label named lbl on the top
+        /// initialize the text of the textbox named textarea in the middle
+        /// </summary>
         public OrderPanel()
         {
             InitializeComponent();
@@ -26,18 +33,23 @@ namespace  ConversationAPI
             txtarea.Text = "Robot: Hi,how can I help you?";
         }
 
+        /// <summary>
+        /// send button click event
+        /// this event will get the value of the GetResponse method of ConversationHelper class
+        /// get the substring of the return value and give the value to the string "Ru"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btn_Click(object sender, EventArgs e)
         {
             input = txtbox.Text;
-            var res = Task.Run(() => helper.GetResponse(input).GetAwaiter().GetResult()).Result;  
+            var res = Task.Run(() => helper.GetResponse(input).GetAwaiter().GetResult()).Result;  //run the statement after the task finish
             string strtempa = "output\":{\"text\":[\"";
             string strtempb = "\"],\"nodes_visited";
             int IndexofA = res.IndexOf(strtempa);
             int IndexofB = res.IndexOf(strtempb);
-            string Ru = res.Substring(IndexofA + 18, IndexofB - IndexofA - 18);
-
-            
-
+            string Ru = res.Substring(IndexofA + 18, IndexofB - IndexofA - 18); // get the value of part of the json format result
+            //use the textbox named txtarea to record the message
             if (txtarea.Text.Equals(""))
                 txtarea.Text = "You: " + input + Environment.NewLine + "Robot: " + Ru;
             else
@@ -46,14 +58,15 @@ namespace  ConversationAPI
 
         }
 
+        /// <summary>
+        /// clear button click event
+        /// clear the txtarea compoment
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnClear_Click(object sender, EventArgs e)
         {
             txtarea.Text = "";
-        }
-
-        void t_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
-        {
-            txtarea.Text = txtarea.Text + "\n" + "aaa";
         }
     }
 }
